50916265c5213e70103cd7ec077ecd37661ebe3a
MCAVES <rock.team.381@gmail.com>
2025-01-17 08:21:07 +0545

    users setup page

--- Introductory/Controllers/UsersController.cs
+++ Introductory/Controllers/UsersController.cs
@@ -79,6 +79,7 @@ namespace Introductory.Controllers
                         ContactNo = vm.ContactNo.ToText(),
                         ValidFrom = vm.ValidFrom.ToEnglishDate(),
                         ValidTo = vm.ValidTo.ToEnglishDate(),
+                        IsActive = true
                     };
 
                     _context.Users.Add(dbMdl);
@@ -100,6 +101,11 @@ namespace Introductory.Controllers
         {
             List<UsersVM> dbData = _context
                                     .Users
+                                    .Where(x => 
+                                        x.IsActive == true
+                                        && (string.IsNullOrEmpty(vm.Username) || x.Username.Contains(vm.Username))
+                                        && (string.IsNullOrEmpty(vm.ContactNo) || x.ContactNo.Contains(vm.ContactNo))   
+                                     )
                                     .Select(s => new UsersVM
                                     {
                                         Username = s.Username.ToText(),
@@ -118,6 +124,35 @@ namespace Introductory.Controllers
                 Success = true,
                 Data = dbData
             });
+        }
+
+        [HttpGet]
+        public JsonResult Delete(int id)
+        {
+            var oldData = _context
+                            .Users
+                            .Where(x => x.UserID == id)
+                            .FirstOrDefault();
+
+            if (oldData == null)
+            {
+                return Json(new
+                {
+                    Success = false,
+                    Messaage = "Data Not Found!"
+                });
+            }
+            else
+            {
+                oldData.IsActive = false;
+                _context.SaveChanges();
+
+                return Json(new
+                {
+                    Success = true,
+                    Messaage = "User Deleted Successfully!"
+                });
+            }
         }
     }
 }
