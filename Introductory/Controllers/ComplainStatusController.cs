9b4e884f7ad75cfa6d24ee668af0b8bb1fab2ab9
MCAVES <rock.team.381@gmail.com>
2025-01-23 08:24:48 +0545

    to keep latest

--- Introductory/Controllers/ComplainStatusController.cs
+++ Introductory/Controllers/ComplainStatusController.cs
@@ -42,38 +42,70 @@ namespace Introductory.Controllers
             }
             else
             {
-                var oldComplain = _context
-                                .ComplainStatus
-                                .Where(x => x.ComplainStatusCode == vm.ComplainStatusCode && x.CreatedBy == vm.CreatedBy)
-                                .FirstOrDefault();
-                if (oldComplain != null)
+                if (vm.ComplainStatusID == 0)
                 {
-                    return Json(new
+                    // ADD NEW ROW
+                    var oldComplain = _context
+                                    .ComplainStatus
+                                    .Where(x => x.ComplainStatusCode == vm.ComplainStatusCode)
+                                    .FirstOrDefault();
+                    if (oldComplain != null)
                     {
-                        Success = false,
-                        Message = "Complain Status Already Exists"
-                    });
+                        return Json(new
+                        {
+                            Success = false,
+                            Message = "Complain Status Already Exists"
+                        });
+                    }
+                    else
+                    {
+                        ComplainStatus dbMdl = new ComplainStatus()
+                        {
+                            ComplainStatusID = vm.ComplainStatusID,
+                            ComplainStatusName = vm.ComplainStatusName.ToText(),
+                            ComplainStatusCode = vm.ComplainStatusCode.ToText(),
+                            CreatedDate = DateTime.Now,
+                            CreatedBy = LoggedInUserID,
+                            IsActive = true,
+                        };
+
+                        _context.ComplainStatus.Add(dbMdl);
+
+                        _context.SaveChanges();
+                        return Json(new
+                        {
+                            Success = true,
+                            Message = "Complain Status Registered Successfully!!!"
+                        });
+                    }
                 }
                 else
                 {
-                    ComplainStatus dbMdl = new ComplainStatus()
-                    { 
-                        ComplainStatusID = vm.ComplainStatusID,
-                        ComplainStatusName = vm.ComplainStatusName.ToText(),
-                        ComplainStatusCode = vm.ComplainStatusCode.ToText(),
-                        CreatedDate = DateTime.Now,
-                        CreatedBy = LoggedInUserID,
-                        IsActive = true,
-                    };
-
-                    _context.ComplainStatus.Add(dbMdl);
-
-                    _context.SaveChanges();
-                    return Json(new
+                    // UPDATE EXISTING ROWS
+                    var oldRow = _context.ComplainStatus
+                                    .Where(x => x.ComplainStatusID == vm.ComplainStatusID)
+                                    .FirstOrDefault();
+                    if (oldRow == null)
                     {
-                        Success = true,
-                        Message = "Complain Status Registered Successfully!!!"
-                    });
+                        return Json(new
+                        {
+                            Success = false,
+                            Message = "Complain Status Not Found"
+                        });
+                    }
+                    else
+                    {
+                        oldRow.ComplainStatusName = vm.ComplainStatusName.ToText();
+                        oldRow.ComplainStatusCode = vm.ComplainStatusCode.ToText();
+
+                        _context.SaveChanges();
+
+                        return Json(new
+                        {
+                            Success = true,
+                            Message = "Complain Status Updated Successfuly"
+                        });
+                    }
                 }
             }
         }
@@ -108,32 +140,32 @@ namespace Introductory.Controllers
                 });
             }
         }
-    
 
 
 
 
 
-       [HttpPost]
+
+        [HttpPost]
         public JsonResult GetAllData([FromBody] ComplainStatusVM vm)
         {
-              var dbData = _context
-                               .ComplainStatus
-                                   .Where(x =>
-                                        x.IsActive == true
-                                        && (string.IsNullOrEmpty(vm.ComplainStatusName) || x.ComplainStatusName.Contains(vm.ComplainStatusName))
-                                        && (string.IsNullOrEmpty(vm.ComplainStatusCode) || x.ComplainStatusCode.Contains(vm.ComplainStatusCode))
-                                     )
-                                    .Select(s => new ComplainStatusVM
-                                    {
-                                        ComplainStatusID = s.ComplainStatusID.ToInt32(),
-                                        ComplainStatusName = s.ComplainStatusName.ToText(),
-                                        ComplainStatusCode = s.ComplainStatusCode.ToText(),
-                                        CreatedDate = DateTime.Now,
-                                        CreatedBy = LoggedInUserID,
+            var dbData = _context
+                             .ComplainStatus
+                                 .Where(x =>
+                                      x.IsActive == true
+                                      && (string.IsNullOrEmpty(vm.ComplainStatusName) || x.ComplainStatusName.Contains(vm.ComplainStatusName))
+                                      && (string.IsNullOrEmpty(vm.ComplainStatusCode) || x.ComplainStatusCode.Contains(vm.ComplainStatusCode))
+                                   )
+                                  .Select(s => new ComplainStatusVM
+                                  {
+                                      ComplainStatusID = s.ComplainStatusID.ToInt32(),
+                                      ComplainStatusName = s.ComplainStatusName.ToText(),
+                                      ComplainStatusCode = s.ComplainStatusCode.ToText(),
+                                      CreatedDate = DateTime.Now.ToShortDateString(),
+                                      CreatedBy = LoggedInUserID,
 
-                                    })
-                                    .ToList();
+                                  })
+                                  .ToList();
             return Json(new
             {
                 Success = true,
